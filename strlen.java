044240ed726205f4ed0f579bda783f559cb8add8
satheesh kannan <smarteye9626@gmail.com>
2017-07-05 12:09:20 +0530

    Create strlen.java

---
+++ strlen.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class strlen
+{
+public static void main(String args[])
+{
+Scanner ob=new Scanner(System.in);
+String n;
+n=ob.nextLine();
+int t=n.length();
+System.out.println(t);
+}
+}
